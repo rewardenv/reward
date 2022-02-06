17956c029e41f7efbd809f7f4ed44e428e6ad6d5
Janos Miko <mixe3y@gmail.com>
2022-02-06 13:52:21 +0100

    add support for shell user

--- internal/commands/shell.go
+++ internal/commands/shell.go
@@ -11,14 +11,21 @@ var (
 	ShellCommand []string
 	// ShellContainer is the container used for shell command.
 	ShellContainer          string
-	defaultShellCommand     = "bash"
+	DefaultShellCommand     = "bash"
 	defaultShellCommandsMap = map[string]string{
-		"default":    "bash",
-		"pwa-studio": "sh",
+		"php-fpm": "bash",
+		"default": "sh",
 	}
 	defaultShellContainersMap = map[string]string{
-		"default":    "php-fpm",
 		"pwa-studio": "node",
+		"default":    "php-fpm",
+	}
+
+	ShellUser    = ""
+	shellUserMap = map[string]string{
+		"php-fpm":    "www-data",
+		"pwa-studio": "node",
+		"default":    "root",
 	}
 )
 
@@ -30,21 +37,24 @@ func ShellCmd(cmd *cobra.Command, args []string) error {
 		if ShellContainer == defaultShellContainersMap["default"] {
 			SetShellContainer(defaultShellContainersMap[core.GetEnvType()])
 		}
-
-		SetDefaultShellCommand(defaultShellCommandsMap[core.GetEnvType()])
 	}
+
+	SetDefaultShellCommand(ShellContainer)
+	SetShellUser(ShellContainer)
 
 	if len(args) > 0 {
 		ShellCommand = core.ExtractUnknownArgs(cmd.Flags(), args)
 	} else {
-		ShellCommand = core.ExtractUnknownArgs(cmd.Flags(), []string{defaultShellCommand})
+		ShellCommand = core.ExtractUnknownArgs(cmd.Flags(), []string{DefaultShellCommand})
 	}
 
 	log.Debugln("command:", ShellCommand)
 	log.Debugln("container:", ShellContainer)
 
 	var passedArgs []string
-	passedArgs = append(passedArgs, "exec", ShellContainer)
+	passedArgs = append(passedArgs, "exec")
+	passedArgs = append(passedArgs, "--user", ShellUser)
+	passedArgs = append(passedArgs, ShellContainer)
 	passedArgs = append(passedArgs, ShellCommand...)
 
 	err := EnvRunDockerCompose(passedArgs, false)
@@ -62,5 +72,27 @@ func SetShellContainer(s string) {
 
 // SetDefaultShellCommand changes the command invoked by reward shell command.
 func SetDefaultShellCommand(s string) {
-	defaultShellCommand = s
+	if DefaultShellCommand == "" {
+		if keyExists(defaultShellCommandsMap, s) {
+			DefaultShellCommand = defaultShellCommandsMap[s]
+		} else {
+			DefaultShellCommand = defaultShellCommandsMap["default"]
+		}
+	}
+}
+
+// SetShellUser changes the user of the reward shell command.
+func SetShellUser(s string) {
+	if ShellUser == "" {
+		if keyExists(shellUserMap, s) {
+			ShellUser = shellUserMap[s]
+		} else {
+			ShellUser = shellUserMap["default"]
+		}
+	}
+}
+
+func keyExists(decoded map[string]string, key string) bool {
+	val, ok := decoded[key]
+	return ok && val != ""
 }
