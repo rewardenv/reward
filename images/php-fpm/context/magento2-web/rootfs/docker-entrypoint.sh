47c22f9f3c0bf6efa084957294d1162d05e14884
Janos Miko <mixe3y@gmail.com>
2022-02-10 09:13:34 +0100

    update magento2-web container

--- images/php-fpm/context/magento2-web/rootfs/docker-entrypoint.sh
+++ images/php-fpm/context/magento2-web/rootfs/docker-entrypoint.sh
@@ -80,6 +80,15 @@ fi
 #  alternatives --set composer /usr/bin/composer2
 #fi
 
-set -- supervisord -c /etc/supervisor/supervisord.conf
+# If the first arg is `-D` or `--some-option` pass it to php-fpm.
+if [ $# -eq 0 ] || [ "${1#-}" != "$1" ] || [ "${1#-}" != "$1" ]; then
+  set -- supervisord -c /etc/supervisor/supervisord.conf "$@"
+# If the first arg is supervisord call it normally.
+elif [ "${1}" == "supervisord" ]; then
+  set -- "$@"
+# If the first arg is anything else
+else
+  set -- "$@"
+fi
 
 exec "$@"
