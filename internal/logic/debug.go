523f7ea4ea5fa86cc3caa79dd78adfb4aa47ef5e
Janos Miko <mixe3y@gmail.com>
2023-03-07 09:21:30 +0100

    fix an issue with xdebug remote host, change the default debug host to host.docker.internal

--- internal/logic/debug.go
+++ internal/logic/debug.go
@@ -18,21 +18,21 @@ func (c *Client) RunCmdDebug(cmd *cobra.Command, args []string) error {
 		command = util.ExtractUnknownArgs(cmd.Flags(), []string{"bash"})
 	}
 
-	debugHost, err := c.Docker.ContainerGatewayInNetwork("php-debug", c.EnvNetworkName())
-	if err != nil {
-		return fmt.Errorf("error getting debug container gateway: %w", err)
-	}
+	// debugHost, err := c.Docker.ContainerGatewayInNetwork("php-debug", c.EnvNetworkName())
+	// if err != nil {
+	// 	return fmt.Errorf("error getting debug container gateway: %w", err)
+	// }
 
 	passedArgs := append(
 		[]string{
 			"exec",
 			"-e",
-			fmt.Sprintf("XDEBUG_REMOTE_HOST=%s", debugHost),
+			// fmt.Sprintf("XDEBUG_REMOTE_HOST=%s", debugHost),
 			"php-debug",
 		}, command...,
 	)
 
-	err = c.RunCmdEnvDockerCompose(passedArgs, shell.WithCatchOutput(false))
+	err := c.RunCmdEnvDockerCompose(passedArgs, shell.WithCatchOutput(false))
 	if err != nil {
 		return fmt.Errorf("error running docker compose command: %w", err)
 	}
